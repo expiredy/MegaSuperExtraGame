29646c446a47b780ed002b1fb597dd0681f4d815
expiredy101 <62770020+expiredy101@users.noreply.github.com>
2021-01-08 02:16:38 +0300

    OH yeah ANIMATIONS HAD COME TO US, (I really don't wanna see it again)

--- button.py
+++ button.py
@@ -3,13 +3,15 @@ import pygame
 
 class Button():
     button_group = None
+    animation_max_frame = 15
     def __init__(self, button_text, x_cord, y_cord, x_lenth, y_lenth, func=None, new_button_group=None,
-                 text_color=(255, 0, 0), font_size=40, font_for_text="Earth's Mightiest Bold Cyrillic",
+                 text_color=(255, 0, 0), font_size=90, font_for_text="Lilita One Russian",
                  background=(55,55,255), backgroynd_tex=None, color_of_outline=None,
                  outline_lenth=None):
         self.button_text = str(button_text)
         self.x_cord, self.y_cord, self.x_lenth, self.y_lenth = x_cord, y_cord, x_lenth, y_lenth
         self.color_of_outline, self.outline_lenth = color_of_outline, outline_lenth
+        self.is_animation_started = False
         try:
             self.x_cord, self.y_cord = x_cord, y_cord
         except ValueError:
@@ -24,6 +26,7 @@ class Button():
         if func:
             self.main_function = func
         self.text_color = text_color
+        self.present_animation_frame = 0
         self.background = background if not backgroynd_tex else backgroynd_tex
         self.font_for_text, self.font_size = font_for_text, font_size
 
@@ -39,12 +42,21 @@ class Button():
         pass
 
     def target_animation(self):
-        pass
+        if self.is_animation_started and self.present_animation_frame < self.animation_max_frame:
+                self.present_animation_frame += 1
+                self.font_size += 1
+        if not self.is_animation_started and self.present_animation_frame > 0:
+            self.present_animation_frame -= 1
+            self.font_size -= 1
 
     def is_targeted(self, pos):
         if self.x_cord <= pos[0] <= self.x_cord + self.x_lenth and self.y_cord <= pos[1] <= self.y_cord + self.y_lenth:
+            self.is_animation_started = True
             return True
-        return False
+        else:
+            self.target_animation()
+            self.is_animation_started = False
+            return False
 
     def draw(self, canvas):
         pygame.font.init()
@@ -63,7 +75,7 @@ class Button():
 
 class TextButton(Button):
     def __init__(self, button_text, x_cord, y_cord, x_lenth, y_lenth, func=None, new_button_group=None,
-                 text_color=(255, 0, 0), font_size=40, font_for_text='Comic Sans MS', background=(55,55,255), backgroynd_tex=None, color_of_outline=None,
+                 text_color=(255, 0, 0), font_size=40, font_for_text="Rockin' Record", background=(55,55,255), backgroynd_tex=None, color_of_outline=None,
                  outline_lenth=None):
         super().__init__()
 
