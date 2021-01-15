3fd4f49b7fc55fb9a83a45a489c705a9d0584cbb
Neroune <73380022+Neroune@users.noreply.github.com>
2021-01-16 01:25:12 +0300

    add first minigame

---
+++ lab.py
@@ -0,0 +1,124 @@
+import os
+import pygame
+
+
+class Player(object):
+
+    def __init__(self):
+        self.rect = pygame.Rect(32, 32, 16, 16)
+
+    def move(self, dx, dy):
+
+        if dx != 0:
+            self.move_single_axis(dx, 0)
+        if dy != 0:
+            self.move_single_axis(0, dy)
+
+    def move_single_axis(self, dx, dy):
+
+        self.rect.x += dx
+        self.rect.y += dy
+
+        for wall in walls:
+            if self.rect.colliderect(wall.rect):
+                if dx > 0:
+                    self.rect.right = wall.rect.left
+                if dx < 0:
+                    self.rect.left = wall.rect.right
+                if dy > 0:
+                    self.rect.bottom = wall.rect.top
+                if dy < 0:
+                    self.rect.top = wall.rect.bottom
+
+
+class Wall(object):
+
+    def __init__(self, pos):
+        walls.append(self)
+        self.rect = pygame.Rect(pos[0], pos[1], 16, 16)
+
+
+os.environ["SDL_VIDEO_CENTERED"] = "1"
+pygame.init()
+
+pygame.display.set_caption("ярик не бей")
+screen = pygame.display.set_mode((900, 300))
+
+clock = pygame.time.Clock()
+walls = []
+player = Player()
+pygame.init()
+
+
+
+
+level = [
+    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+    "W    WWW     WWWWW           W  W  W     EW",
+    "W    WWW W       W  WWWWWWW  W      W   w W",
+    "W    W   W   WW  W        W  W  W W     W W",
+    "W    W WW W  W   W  WWWWWWW  W  W  W  W  WW",
+    "W    W   W   W   W  W        W    W W  W  W",
+    "W    WWW W W W      W        W  W     W   W",
+    "W        WWW WW  WWW         W   W   W W  W",
+    "W                                         W",
+    "WWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWWW",
+]
+
+x = y = 0
+for row in level:
+    for col in row:
+        if col == "W":
+            Wall((x, y))
+        if col == "E":
+            end_rect = pygame.Rect(x, y, 16, 16)
+        x += 16
+    y += 16
+    x = 0
+
+    def timer_():
+        counter, text = 10, '10'.rjust(3)
+        pygame.time.set_timer(pygame.USEREVENT, 1000)
+        font = pygame.font.SysFont('Consolas', 30)
+        while True:
+            for e in pygame.event.get():
+                if e.type == pygame.USEREVENT:
+                    counter -= 1
+                    text = str(counter).rjust(3) if counter > 0 else 'слоупок'
+                if e.type == pygame.QUIT: break
+            else:
+                screen.fill((255, 255, 255))
+                screen.blit(font.render(text, True, (0, 0, 0)), (32, 48))
+                pygame.display.flip()
+                clock.tick(60)
+                continue
+            break
+while True:
+    clock.tick(60)
+
+    for e in pygame.event.get():
+        if e.type == pygame.QUIT:
+            running = False
+        if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
+            running = False
+
+    key = pygame.key.get_pressed()
+    if key[pygame.K_LEFT]:
+        player.move(-2, 0)
+    if key[pygame.K_RIGHT]:
+        player.move(2, 0)
+    if key[pygame.K_UP]:
+        player.move(0, -2)
+    if key[pygame.K_DOWN]:
+        player.move(0, 2)
+
+    if player.rect.colliderect(end_rect):
+        raise SystemExit
+
+    screen.fill((0, 0, 0))
+    for wall in walls:
+        pygame.draw.rect(screen, (255, 255, 255), wall.rect)
+    pygame.draw.rect(screen, (255, 0, 0), end_rect)
+    pygame.draw.rect(screen, (255, 200, 0), player.rect)
+    pygame.display.flip()
+
